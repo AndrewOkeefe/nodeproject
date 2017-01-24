ea41b9043654c770706efa8c1960625634b1c9fe
Andrew Okeefe <aokeefe4343@gmail.com>
2017-01-24 01:05:42 +0000

    Added start script and express

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,8 @@
-var aloha  = 'Aloha Variables';
-console.log(aloha)
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+app.listen(port, function(err){
+    console.log("the server is running on port: " + port);
+});
+
